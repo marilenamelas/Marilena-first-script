211b6d429a285a85b829fe28adce81d7a3ee564c
marilenamelas <31265217+marilenamelas@users.noreply.github.com>
2017-08-31 21:54:59 -0700

    Add files via upload

---
+++  script.sh
@@ -0,0 +1,10 @@
+# use /bin/bash to run commands, instead of the default /bin/sh
+SHELL=/bin/bash
+# mail any output to exampleuser, no matter whose crontab this is
+MAILTO=davidwcr@usc.edu  
+# set the PATH variable to make sure all commands in the crontab are found
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+59 23 31 8 4 echo "Coding is cool!"
+
+ 
